180aadf38f87025fa2aae186e2125d218e3ae847
olegatorh <herston4ik@gmail.com>
2022-06-10 17:35:19 +0300

    first commit

---
+++ estete/estate/estate/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('supersecret/', admin.site.urls),
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth/', include('djoser.urls.jwt')),
+    path("api/v1/profile/", include("apps.profiles.urls"))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = 'Real Estate Admin'
+admin.site.site_title = 'Real Estate Admin Portal'
+admin.site.index_title = 'Welcome to the  Real Estate Portal'
